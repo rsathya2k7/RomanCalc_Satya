3cb66f746d11bae7866df5b67eac225014e2c3d8
rsathya2k7 <rsathya2k7@gmail.com>
2016-07-21 22:44:33 +0530

    Update UnitTest.java

--- UnitTest.java
+++ UnitTest.java
@@ -6,47 +6,47 @@ import javax.naming.NamingException;
 
 public class TestForTestCase{
 
-  public static String RomanNumerals(int Int) {
-   String res = "";
+  public static String romanNumerals(int inputValue) {
+   String result = "";
        // check the condition is equal to Zero
      
-        if(Int==0)
+        if(inputValue==0)
         {
-           res="Zero is not Convert into Roman Numeral value";
+           result="Zero is not Convert into Roman Numeral value";
            }
       //if given input value less then Zero then
         
-        else if(Int<=-1)
+        else if(inputValue<=-1)
         {
-            res="Negative value is not Convert into Roman Numeral value";
+            result="Negative value is not Convert into Roman Numeral value";
            }
       //if given input value Grater then 10 then
         
-        else if(Int>10)
+        else if(inputValue>10)
         {
-             res="Please Enter the value as 10";
+             result="Please Enter the value as 10";
            }
        //if given condition is true it convert Arabic Number into Numeral
-       else if(Int==10){
+       else if(inputValue==10){
           LinkedHashMap<String, Integer> roman_numerals=new LinkedHashMap<String,Integer>();
           roman_numerals.put("X", 10);
           for ( Map.Entry<String, Integer> entry : roman_numerals.entrySet()) {
-              res = "Given Arabic Number Converted into Numeric : "+Int+"  "+entry.getKey();//System.out.println("key :"+key);
+              result = "Given Arabic Number Converted into Numeric : "+inputValue+"  "+entry.getKey();//System.out.println("key :"+key);
               
           }
           
              }
         
-       return res;
+       return result;
         
   }       
 
 public static void main(String[] args) {
- TestForTestCase t=new TestForTestCase();
- System.out.println("if value is 0 :"+t.RomanNumerals(0));
- System.out.println("if value is -1 :"+t.RomanNumerals(-1));
- System.out.println("if value is 11 :"+t.RomanNumerals(11));
- System.out.println("if value is 10 :"+t.RomanNumerals(10));
+ TestForTestCase test =new TestForTestCase();
+ System.out.println("if value is 0 :"+test.romanNumerals(0));
+ System.out.println("if value is -1 :"+test.romanNumerals(-1));
+ System.out.println("if value is 11 :"+test.romanNumerals(11));
+ System.out.println("if value is 10 :"+test.romanNumerals(10));
 }
 
 }
